92cf8efad9c11b2dfbf68387d34532c81b944213
Jehee Cheon <jeheecheon@gmail.com>
2024-06-12 08:40:57 +0900

    Updates

--- assets/Avatar-_n7UfGNr.js
+++ assets/Avatar-_n7UfGNr.js
@@ -1,3 +1,3 @@
-import{j as s}from"./index-8bumPwoh.js";const n=({className:e,avatar:r,size:a=45,children:d})=>s.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?s.jsx("img",{src:r,className:`rounded-full
+import{j as s}from"./index-0Bw4pZfj.js";const n=({className:e,avatar:r,size:a=45,children:d})=>s.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?s.jsx("img",{src:r,className:`rounded-full
                             ${e} `,width:a}):d&&s.jsx("div",{className:`border-[1px] bg-default-9-dark dark:border-default-10-dark\r
                         ring-[1.5px] ring-orange-300 rounded-full`,children:d})});export{n as A};
