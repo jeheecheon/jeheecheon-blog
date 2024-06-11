61643224a7bfc39b716712cf50bce13a1e1194ef
Jehee Cheon <jeheecheon@gmail.com>
2024-06-12 08:04:22 +0900

    Updates

--- assets/Avatar-JcPeX0jT.js
+++ assets/Avatar-JcPeX0jT.js
@@ -1,3 +1,3 @@
-import{j as s}from"./index-_LnhoYLO.js";const n=({className:e,avatar:r,size:a=45,children:d})=>s.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?s.jsx("img",{src:r,className:`rounded-full
+import{j as s}from"./index-lA7i9Pwl.js";const n=({className:e,avatar:r,size:a=45,children:d})=>s.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?s.jsx("img",{src:r,className:`rounded-full
                             ${e} `,width:a}):d&&s.jsx("div",{className:`border-[1px] bg-default-9-dark dark:border-default-10-dark\r
                         ring-[1.5px] ring-orange-300 rounded-full`,children:d})});export{n as A};
