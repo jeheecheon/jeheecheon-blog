0e3dfbe0b4e715e6dd27c76d16b5ccec7e505273
Jehee Cheon <jeheecheon@gmail.com>
2024-06-19 19:23:47 +0900

    Updates

--- assets/Avatar-bOnu4TbX.js
+++ assets/Avatar-bOnu4TbX.js
@@ -1,3 +1,3 @@
-import{j as s}from"./index-tOuc_NNz.js";const n=({className:e,avatar:r,size:a=45,children:d})=>s.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?s.jsx("img",{src:r,className:`rounded-full
+import{j as s}from"./index-zIVyL-63.js";const n=({className:e,avatar:r,size:a=45,children:d})=>s.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?s.jsx("img",{src:r,className:`rounded-full
                             ${e} `,width:a}):d&&s.jsx("div",{className:`border-[1px] bg-default-9-dark dark:border-default-10-dark\r
                         ring-[1.5px] ring-orange-300 rounded-full`,children:d})});export{n as A};
