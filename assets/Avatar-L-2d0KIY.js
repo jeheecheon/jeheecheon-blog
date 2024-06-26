f82386667221ec04f01ccf7a30e2afddb06cc06e
jeheecheon <jeheecheon@gmail.com>
2024-06-26 16:33:43 +0900

    Updates

--- assets/Avatar-L-2d0KIY.js
+++ assets/Avatar-L-2d0KIY.js
@@ -1,3 +1,3 @@
-import{j as s}from"./index-Mqsgk-6j.js";const n=({className:e,avatar:r,size:a=45,children:d})=>s.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?s.jsx("img",{src:r,className:`rounded-full
+import{j as s}from"./index-Vr_gb4ZI.js";const n=({className:e,avatar:r,size:a=45,children:d})=>s.jsx("div",{className:"h-fit shrink-0",children:r&&r!==""?s.jsx("img",{src:r,className:`rounded-full
                             ${e} `,width:a}):d&&s.jsx("div",{className:`border-[1px] bg-default-9-dark dark:border-default-10-dark\r
                         ring-[1.5px] ring-orange-300 rounded-full`,children:d})});export{n as A};
